4140ff04d5af398fe205a6682c03af7315706a6f
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-03-19 17:58:38 +0900

    first commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,11 @@
+## 対応内容
+
+## 備考
+
+- [ ] approveの場合はmerge可
+- [ ] npm install 必要
+- [ ] commonパッケージ更新あり
+
+## 対応issue
+
+close #issueNumber
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+}
+
+module.exports = config
